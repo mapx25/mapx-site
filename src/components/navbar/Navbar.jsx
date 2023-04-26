7ecefb5b0285d38477adfc191acbc83d9348f494
mapx25 <130289775+mapx25@users.noreply.github.com>
2023-04-26 17:25:55 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,14 @@ export default function Navbar() {
         <div className='options'>
             <ul className='nav-items'>
                 <a href="#home" className='nav-item'>Home</a>
-                <a href="#about" className='nav-item'>About</a>
+                <div className='dropdown'>
+                  <div href="#about" className='nav-item'>About</div>
+                  <div class="dropdown-content">
+                    <a href="https://drive.google.com/file/d/15jyT2yhWLm5WPAW7BLD5euPTLBiJzyd1/view?usp=share_link">Whitepaper</a>
+                    <a href="https://docs.google.com/presentation/d/1t5SgSFdsC1lIdbNz2XFwdAz_1BWaEl4W/edit?usp=sharing&ouid=106129527907134070426&rtpof=true&sd=true">Pitchdeck</a>
+                    
+                  </div>
+                </div>
                 <a href="#overview" className='nav-item'>Overview</a>
                 <a href="#contact" className='nav-item'>Contact</a>
             </ul>
